f9e7fb3e2d2a25c09536456071f13027ad934e8b
Ji_Subin <83721406+Ji-subin-97@users.noreply.github.com>
2025-01-08 11:01:01 +0900

    [level 1] Title: 과일 장수, Time: 91.61 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    int boxCount = 0;
+    size_t scoreSize = score.size(); 
+
+    priority_queue<int> que;
+    for(int i = 0; i < score.size(); i++) {
+        que.push(score[i]);
+    }
+    
+    size_t index = 0;
+    boxCount = scoreSize / m;
+    while(boxCount > 0) { 
+        if(index == m) {
+            answer += k * m;
+            index = 0;
+            boxCount--;
+        }
+        
+        k = que.top();
+        que.pop();
+        index++;
+    }
+        
+    return answer;
+}
