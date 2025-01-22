25bd496a43babd7de55baf084c9a8847aada6a21
Ji_Subin <83721406+Ji-subin-97@users.noreply.github.com>
2025-01-22 09:41:25 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.41 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> solution(vector<string> keymap, vector<string> targets) {
+    vector<int> answer;
+    unordered_map<char, int> tempMap;
+    
+    for(string keymapItem : keymap)
+    {
+        for(int i = 0; i < keymapItem.length(); i++)
+        {
+            if(tempMap.find(keymapItem[i]) != tempMap.end() && tempMap[keymapItem[i]] > i + 1)
+            {
+                tempMap[keymapItem[i]] = i + 1;  
+            }
+            else
+            {
+                tempMap.insert(make_pair(keymapItem[i], i + 1));  
+            }
+            
+        }
+    }
+    
+    for(string targetItem : targets)
+    {
+        int result = 0;
+        
+        for(int i = 0; i < targetItem.length(); i++)
+        {
+            if(tempMap.find(targetItem[i]) == tempMap.end())
+            {
+                result = 0;
+                break;
+            }
+            
+            result += tempMap[targetItem[i]];  
+        }
+        
+        if(result > 0)
+        {
+            answer.emplace_back(result);  
+        } 
+        else
+        {
+            answer.emplace_back(-1);  
+        }
+    }
+    
+    return answer;
+}
