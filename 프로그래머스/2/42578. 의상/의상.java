d491d08bbf5954c7614be3b7e897ef3b2feec3cf
Ji_Subin <83721406+Ji-subin-97@users.noreply.github.com>
2025-04-09 19:20:12 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String[] item : clothes)
+        {
+            map.put(item[1], map.getOrDefault(item[1], 0) + 1);
+        }
+        
+        for(int count : map.values())
+        {
+            answer *= (count + 1);
+        }
+        
+        return answer - 1;
+    }
+}
