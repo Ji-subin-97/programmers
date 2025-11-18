65e57e1a4a1242d316b88ab88808d7a631b2ce9e
Ji_Subin <83721406+Ji-subin-97@users.noreply.github.com>
2025-11-18 23:14:23 +0900

    [level 0] Title: 모스부호 (1), Time: 0.24 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String letter) {
+        StringBuilder sb = new StringBuilder();
+        Map<String, String> morse = Map.ofEntries(
+            Map.entry(".-", "a"),    Map.entry("-...", "b"),  Map.entry("-.-.", "c"),
+            Map.entry("-..", "d"),   Map.entry(".", "e"),     Map.entry("..-.", "f"),
+            Map.entry("--.", "g"),   Map.entry("....", "h"),  Map.entry("..", "i"),
+            Map.entry(".---", "j"),  Map.entry("-.-", "k"),   Map.entry(".-..", "l"),
+            Map.entry("--", "m"),    Map.entry("-.", "n"),    Map.entry("---", "o"),
+            Map.entry(".--.", "p"),  Map.entry("--.-", "q"),  Map.entry(".-.", "r"),
+            Map.entry("...", "s"),   Map.entry("-", "t"),     Map.entry("..-", "u"),
+            Map.entry("...-", "v"),  Map.entry(".--", "w"),   Map.entry("-..-", "x"),
+            Map.entry("-.--", "y"),  Map.entry("--..", "z")
+        );
+        
+        String[] strc = letter.split(" ");
+        
+        for(String str : strc) {
+            if(morse.get(str) != null) sb.append(morse.get(str));
+        }
+        
+        return sb.toString();
+    }
+}
